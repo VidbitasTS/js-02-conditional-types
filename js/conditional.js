bfc2b3526f11ab86cabb414c934d91768c2ab4a8
vidbitas <vidbitas@gmail.com>
2022-07-05 15:49:12 +0300

    math

--- js/conditional.js
+++ js/conditional.js
@@ -1,9 +1,23 @@
 console.log('conditionals');
 
-let isUserAdmin = prompt('koks amzius');
+// let isUserAdmin = Number(prompt('koks amzius'));
 
-if (isUserAdmin >= 18) {
-    console.log('isUserAdmin ===suauges');
+// if (isUserAdmin >= 18) {
+//     console.log('isUserAdmin ===suauges');
+// } else {
+//     console.log('isUserAdmin ===nepilnametis');
+// }
+
+let amzius;
+
+if (amzius <= 0 || amzius > 150) {
+    console.log('kazkas negerai 1', amzius);
+} else if (amzius > 0 && amzius <= 18) {
+    console.log('vaikas');
+} else if (amzius < 70) {
+    console.log('suauges');
+} else if (amzius < 150) {
+    console.log('senjoras');
 } else {
-    console.log('isUserAdmin ===nepilnametis');
+    console.log('kazkas negerai 2', amzius);
 }
---
+++ js/math.js
@@ -0,0 +1,8 @@
+console.log(Math.PI);
+console.log(Math.sqrt(9));
+let x1 = 11.122;
+let round = Math.round(x1);
+console.log(round);
+
+console.log(Math.pow(3, 3));
+console.log(3 ** 3);
